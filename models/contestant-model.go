c8449a17d5af53e5fc7d18a148af6b876eddc142
Aleksandr Jashhuk <richmanfx@yandex.ru>
2017-05-05 07:22:57 +0300

    - Структура для данных участника. - Матрица диапазонов. - Скрапинг всех данных. - В темплейь вынесены теги таблицы. - В меню темплета с таблицей добавлен пункт для ксрапинга.

---
+++ models/contestant-model.go
@@ -0,0 +1,15 @@
+package models
+
+type Contestant struct {
+	// Названия с заглавных букв - доступны во всём пакете
+	ID			int
+	Call		string
+	QRA			string
+	Band_2m		string
+	Band_70cm	string
+	Band_23cm	string
+	Band_5cm	string
+	Band_3cm	string
+	Band_1cm	string
+	Info		string
+}
--- modules/handlers.go
+++ modules/handlers.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 	"github.com/tebeka/selenium"
 	"time"
+	"strings"
+	"fmt"
+	"strconv"
+	"../models"
 )
 
 func showIndexPage(context *gin.Context)  {
@@ -20,8 +24,6 @@ func showIndexPage(context *gin.Context)  {
 }
 
 func scrapVhfdx(context *gin.Context)  {
-
-
 
 	var webDriver selenium.WebDriver
 	var err error
@@ -45,23 +47,29 @@ func scrapVhfdx(context *gin.Context)  {
 
 	webDriver.MaximizeWindow("")
 
-	// На сайт
-	webDriver.Get("http://www.vhfdx.ru")
-	time.Sleep(2 * time.Second)
+	//// На сайт
+	//webDriver.Get("http://www.vhfdx.ru")
+	//time.Sleep(2 * time.Second)
+	//
+	//// Переходим на страницу Полевого дня.
+	//btn, _ := webDriver.FindElement(selenium.ByXPATH, "//a[@href='http://www.vhfdx.ru/field_day']")
+	//btn.Click()
+	//
+	//time.Sleep(2 * time.Second)
+	//
+	//// Переходим на страницу с таблицей участников.
+	//btn, _ = webDriver.FindElement(selenium.ByXPATH, "//a[contains(text(),'Таблица участников')]")
+	//btn.Click()
+	//time.Sleep(2 * time.Second)
 
-	// Переходим на страницу Полевого дня.
-	btn, _ := webDriver.FindElement(selenium.ByXPATH, "//a[@href='http://www.vhfdx.ru/field_day']")
-	btn.Click()
 
-	time.Sleep(2 * time.Second)
+	webDriver.SetAsyncScriptTimeout(10 * time.Second)		// Таймаут ожидания элемента
 
-	// Переходим на страницу с таблицей участников.
-	btn, _ = webDriver.FindElement(selenium.ByXPATH, "//a[contains(text(),'Таблица участников')]")
-	btn.Click()
+	webDriver.Get("http://www.vhfdx.ru/component/option,com_fabrik/Itemid,307/")
 	time.Sleep(2 * time.Second)
 
 	// Проверяем отображение страницы с таблицей участников.
-	btn, _ = webDriver.FindElement(selenium.ByXPATH, "//title[contains(text(),'Российский УКВ портал - ПД  2016')]")
+	btn, _ := webDriver.FindElement(selenium.ByXPATH, "//title[contains(text(),'Российский УКВ портал - ПД  2016')]")
 	btn.Click()
 	time.Sleep(2 * time.Second)
 
@@ -72,13 +80,15 @@ func scrapVhfdx(context *gin.Context)  {
 
 
 	// Все строки с позывными со всех страниц в слайс
-	overallResult := make([]string, 0, 200)
+
+	maxCountContestant := 200		// Максмальное количество участников соревнований, слайс растянется при необходимости
+	overallResult := make([]string, 0, maxCountContestant)
 	for {
 		// Считать данные с одной страницы
-		var strings []string = readDateFromPage(webDriver)
+		var contestantStrings []string = readDateFromPage(webDriver)
 
 		// Добавить к общему результату всех страниц
-		for _, str := range strings {
+		for _, str := range contestantStrings {
 			if str != "" {
 				//fmt.Println(str)
 				overallResult = append(overallResult, str)
@@ -94,12 +104,11 @@ func scrapVhfdx(context *gin.Context)  {
 			// Переходим на следующую страницу.
 			btn, _ = webDriver.FindElement(selenium.ByXPATH, "//a[@class='pagenav' and @title='Следующая']")
 			btn.Click()
+			time.Sleep(5 * time.Second)
 		} else {
 			break
 		}
 	}
-
-
 
 	var title, _ = webDriver.Title()
 	//time.Sleep(10 * time.Second)
@@ -108,32 +117,153 @@ func scrapVhfdx(context *gin.Context)  {
 	webDriver.Quit()
 
 
+	/// Обработка результатов
+	// Генерация матрицы диапазонов
+	overallResult = makeBandMatrix(overallResult)
+
+	// Вычисление расстояния
+
+	// Вычисление азимута
+
+	// Вычисление обратного азимута
+
+	// Получить всех участников
+	contestantCount := len(overallResult)	// Количесво участников
+	contestantList := make([]models.Contestant, contestantCount)
+	toHTMLTable(overallResult, contestantList)
+
 	context.HTML(
 		http.StatusOK,
 		"vhfdx.html",
 		gin.H{
 			"title": title,
-			"callStrings": overallResult,
+			"contestantCount": contestantCount,
+			"contestant": contestantList,
 		},
 	)
 }
 
+
+// Внести данные об участнике в структуру для контекста темплейта
+func toHTMLTable(allContestant []string, contestantList []models.Contestant) {
+	for idx, str := range allContestant {
+		contestantList[idx].ID, _ = strconv.Atoi(strings.Split(str, "||")[0])
+		contestantList[idx].Call = strings.Split(str, "||")[1]
+		contestantList[idx].QRA = strings.Split(str, "||")[2]
+		contestantList[idx].Band_2m = strings.Split(str, "||")[3]
+		contestantList[idx].Band_70cm = strings.Split(str, "||")[4]
+		contestantList[idx].Band_23cm = strings.Split(str, "||")[5]
+		contestantList[idx].Band_5cm = strings.Split(str, "||")[6]
+		contestantList[idx].Band_3cm = strings.Split(str, "||")[7]
+		contestantList[idx].Band_1cm = strings.Split(str, "||")[8]
+		contestantList[idx].Info = strings.Split(str, "||")[9]
+	}
+}
+
+
+// Создаёт матрицу диапазонов
+func makeBandMatrix(allContestant []string) []string {
+
+	maxCountContestant := 200	// Максмальное количество участников соревнований, слайс растянется при необходимости
+	outputStringWithBandMatrix := make([]string, 0, maxCountContestant)
+
+	for idx, str := range allContestant {
+		bandString := ""
+
+		// Парсим диапазоны
+		bands := strings.Split(str, "||")[2]
+
+		info := strings.Split(str, "||")[3]
+		if info == "" {							// Если ячейка с 'Дополнительной информацией' пуста
+			info = " "
+		}
+
+		if strings.Contains(bands, "144МГц") {
+			bandString += "2м"
+		} else {
+			bandString += "-"
+		}
+
+		if strings.Contains(bands, "432МГц") {
+			bandString += "||70см"
+		} else {
+			bandString += "||-"
+		}
+
+		if strings.Contains(bands, "1296МГц") {
+			bandString += "||23см"
+		} else {
+			bandString += "||-"
+		}
+
+		if strings.Contains(bands, "5.7ГГц") {
+			bandString += "||5см"
+		} else {
+			bandString += "||-"
+		}
+
+		if strings.Contains(bands, "10ГГц") {
+			bandString += "||3м"
+		} else {
+			bandString += "||-"
+		}
+
+		if strings.Contains(bands, "24ГГц") {
+			bandString += "||1.2см"
+		} else {
+			bandString += "||-"
+		}
+
+		outputStringWithBandMatrix = append(outputStringWithBandMatrix,
+			strconv.Itoa(idx + 1) + "||" +
+			strings.Split(str, "||")[0] + "||" +
+			strings.Split(str, "||")[1] + "||" +
+			bandString + "||" +
+			info)
+	}
+	return outputStringWithBandMatrix
+}
+
+
 // Считывает данные участников с одной страницы сайта
 func readDateFromPage(webDriver selenium.WebDriver) []string{
 
+	callsOnPage := 100		// Максимальное лоличество позывных на одной страниц
 	// Позывные
-	elements := make([]selenium.WebElement, 100)
+	callElements := make([]selenium.WebElement, callsOnPage)
+	callElements, _ = webDriver.FindElements(selenium.ByXPATH,
+		"//tr[@class='oddrow0 fabrik_row ' or @class='oddrow1 fabrik_row ']/td[contains(@class,'call')]",)
 
-	elements, _ = webDriver.FindElements(
-		selenium.ByXPATH,
-		"//tr[@class='oddrow0 fabrik_row ' or @class='oddrow1 fabrik_row ']/td[contains(@class,'call')]",
-	)
+	// Квадраты
+	qraElements := make([]selenium.WebElement, callsOnPage)
+	qraElements, _ = webDriver.FindElements(selenium.ByXPATH,
+		"//tr[@class='oddrow0 fabrik_row ' or @class='oddrow1 fabrik_row ']/td[contains(@class,'qra')]")
 
-	result := make([]string, 100)
+	// Диапазоны
+	bandsElements := make([]selenium.WebElement, callsOnPage)
+	bandsElements, _ = webDriver.FindElements(selenium.ByXPATH,
+		"//tr[@class='oddrow0 fabrik_row ' or @class='oddrow1 fabrik_row ']/td[contains(@class,'band')]")
 
-	for index, item := range elements {
-		result[index], _ = item.Text()
+	// Дополнительная информация
+	infoElements := make([]selenium.WebElement, callsOnPage)
+	infoElements, _ = webDriver.FindElements(selenium.ByXPATH,
+		"//tr[@class='oddrow0 fabrik_row ' or @class='oddrow1 fabrik_row ']/td[contains(@class,'info')]")
+
+
+	result := make([]string, callsOnPage)
+
+	for i := 0; i < len(callElements); i++ {
+
+		call, _ := callElements[i].Text()
+		qra, _ := qraElements[i].Text()
+		bands, _ := bandsElements[i].Text()
+		info, _ := infoElements[i].Text()
+
+		result[i] = fmt.Sprintf("%s||%s||%s||%s", 
+			strings.ToTitle(strings.Replace(call, " ", "", -1)),		// Всё в верхний регистр и убрать пробелы
+			strings.ToTitle(strings.Replace(qra, " ", "", -1)), 
+			bands, 
+			info)
 	}
-
 	return result
 }

2032b165c20aa74647c1c388d34367e148c4477e
Aleksandr Yashchuk <Aleksandr.Yashchuk@redsys.ru>
2017-06-01 18:53:07 +0300

    Сообщение в модальном окне об ожидании загрузки данных. Блокировка выполнения при пустом поле ввода QRA (JS).

--- js/apps-scripts.js
+++ js/apps-scripts.js
@@ -1,7 +1,7 @@
 // Таблица
 jQuery(document).ready(function($) {
     $("#hamTable").tablesorter({
-        sortList:[[1,0]],            // Первоначальная сортировка - по позывным
+        sortList:[[1,0]],               // Первоначальная сортировка - по позывным
         widgets:['zebra'],              // Серые строки через одну
         headers:{
             3:{sorter:false},
@@ -12,6 +12,26 @@ jQuery(document).ready(function($) {
             11:{sorter:false},
             12:{sorter:false},
             13:{sorter:false}
+        }
+    });
+});
+
+// Если поле 'my_qra_cup' формы 'cup_form' пустое, алерт и по ссылкам не переходить
+$(function(){
+    $(".russian-cup").click(function() {
+        if(document.cup_form.my_qra_cup.value.length === 0) {
+            alert("Нужно ввести свой QRA!");
+            return false;       //предотвращаем переход по ссылке href или data-target
+        }
+    });
+});
+
+// Если поле 'my_qra_fd' формы 'fd_form' пустое, алерт и по ссылкам не переходить
+$(function(){
+    $(".field-day").click(function() {
+        if(document.fd_form.my_qra_fd.value.length === 0) {
+            alert("Нужно ввести свой QRA!");
+            return false;       //предотвращаем переход по ссылке href или data-target
         }
     });
 });
